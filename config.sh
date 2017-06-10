8c40bdb44e69e5e10a8267bd550f03318400d6af
Moises Perez <moises@perez.lt>
2017-06-10 02:36:34 -0500

    Create config.sh

--- config.sh
+++ config.sh
@@ -27,7 +27,7 @@
 
 # This will be the folder name under /magisk
 # This should also be the same as the id in your module.prop to prevent confusion
-MODID=iYTBPMagisk
+MODID=iYTBPforMagisk
 
 # Set to true if you need to enable Magic Mount
 # Most mods would like it to be enabled
@@ -50,7 +50,7 @@ LATESTARTSERVICE=false
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "Ad-free YouTube Magisk iYTBP"
+  ui_print "iYTBP Magisk Module"
   ui_print "*******************************"
 }
 
